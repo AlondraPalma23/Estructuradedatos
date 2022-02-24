ed1fb9f473600634b363f6d064455dd0e76586dd
AlondraPalma23 <al066187@uacam.mx>
2022-02-23 22:18:20 -0600

    new comit

--- src/estructuradedatos/Estructuradedatos.java
+++ src/estructuradedatos/Estructuradedatos.java
@@ -72,81 +72,168 @@ public class Estructuradedatos {
             System.out.println("La cantidad de mediciones no se encuentra en el rango");
         }
     }  
+
     public static void Tarea1_1_2() {
         separador();
         System.out.println("-----------------[Tarea 1.1.2]--------------------");
         Scanner teclado = new Scanner(System.in);
-        String nombre;
+        String nombre = null;
+        int contN = 0;
+        int Num = 0;
+        String resp = null ;
         String gene ;
-        int cant= 0;
-        int colum = 2;
-        int nn = 0;
-        int band = 0;
-        int max = 0 ;
-        int a  = 0;
-        int b = 0 ;
         
         
-        System.out.println("Ingrese la cantidad de alumnos");
-        cant = teclado.nextInt();
+//        System.out.println("Ingrese el numero de alumnos");
+//        Num = teclado.nextInt();
         
-        String[] alumMusic = new String[cant];
+        int i = 0;
+        String[][] alumMusic = new String[i][2];
+        do {
+              
+            System.out.println("Ingrese los nombres" + "[" + i + "]");
+            alumMusic[i][0] = teclado.next();
+            contN++;
+            
+            System.out.println("Ingrese su genero F=femenino o M= masculino " );
+            alumMusic[i][1] = teclado.next();
+            System.out.println("¿Agregar otro alumno si/no?");
+            resp = teclado.next();
+        } while ("si".equals(resp));
+        
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
+                
+            }
+        }
+        
+//        if (alumMusic[i].compareTo("F")) {
+//            
+//        }
+        
+//        System.out.println(alumMusic[1][0] + alumMusic[i][1]);
+//        for (int j = 0; j < alumMusic[i][0].length(); j++) {
+//            for (int k = 0; k < alumMusic[i][1].length(); k++) {
+//              System.out.println(nombre + gene);  
+//            }
+//        }
+        
+        
+//        for (int i = 0; i < Num; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.println("Ingrese los nombres" + "[" + i + "]");
+//                nombre = teclado.next();
+//                alumMusic[i][0] = nombre;
+//                System.out.println("Ingrese su genero F=femenino o M= masculino ");
+//                gene = teclado.next();
+//                System.out.println("¿Agregar otro alumno si/no?");
+//                resp = teclado.next();
+//            }
+//        }
+
+        
+        
+
+//        System.out.println("nel" + contN);
+        
+        
+        
+        
+//        for (int i = 0; i <= alumMusic.length; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.println("¿cual es tu nombre?");
+//                alumMusic[i][0] = teclado.next();
+//                contN++; 
+//                System.out.println("Ingrese su genero F=femenino o M= masculino ");
+//                gene = teclado.next();
+//                if () {
+//                    
+//                }
+//            }
+//        }    
+        
+//        while (contN > 0 && contN <= 10) {  
+//            System.out.println("¿cual es tu nombre?");
+//            nombre = teclado.next();
+//            contN++;
+//            System.out.println("Ingrese su genero F=femenino o M= masculino ");
+//            gene = teclado.next();
+//            
+//        }
+//        for (int i = 0; i <= alumMusic.length; i++) {
+//            for (int j = 0; j < alumMusic.length; j++) {
+//
+//                
+//                
+//                System.out.println("Ingrese su genero F=femenino o M= masculino ");
+//                alumMusic[i][1] = teclado.next();
+//            }
+//        }           
+      
+        
+         
+
+        
+        
+        
+        
+        
+        //String[][] alumMusic = new String[cant][2];
         //String [] genero = new String[2];
         
-        if (cant <= 10) {
-            for (int i = 0; i < alumMusic.length ; i++) {
-//                for (int j = 0; j < alumMusic.length ; j++) {
-                    System.out.println("¿cual es tu nombre?");
-                    nombre = teclado.next();
+//        if (cant <= 10) {
+//            for (int i = 0; i < alumMusic.length ; i++) {
+////                for (int j = 0; j < alumMusic.length ; j++) {
+//                    System.out.println("¿cual es tu nombre?");
+//                    nombre = teclado.next();
+//                    
+//                    //for (int k = 0; k < 2; k++) {
+//                    System.out.println("Ingrese su genero F=femenino o M= masculino ");
+//                    gene = teclado.next();
+//            }
                     
-                    //for (int k = 0; k < 2; k++) {
-                    System.out.println("Ingrese su genero F=femenino o M= masculino ");
-                    gene = teclado.next();
-                    //}
-                    
+//                if (nn == 0) {
+//                    nn = 1;
+//                    alumMusic[0]  = nombre;
+//                } else { 
+//                    band = 0;
+//                    
+//                    i = -1;
+//                    
+//                    do { 
+//                       i = i + 1; 
+//                       
+//                        if (nombre == alumMusic[i]) {
+//                            band = 1 ;
+//                            a =  max + 1 ;
+//                            b = max;
+//                            
+//                            do {
+//                                alumMusic[a] = alumMusic[b];
+//                                a = a - 1;
+//                                b = b - 1;
+//                            } while (a == i);
+//                        }else{ 
+//                            
+//                        }
+//                    } while (i == max || band == 1);
+//                    
+//                    max = max + 1;
+//                    
+//                    if (band == 1 ) {
+//                        alumMusic[i] = nombre;
+//                    } else {
+//                        alumMusic[max] = nombre;
+//                    }
 //                }
-                if (nn == 0) {
-                    nn = 1;
-                    alumMusic[0]  = nombre;
-                } else { 
-                    band = 0;
-                    
-                    i = -1;
-                    
-                    do { 
-                       i = i + 1; 
-                       
-                        if (nombre == alumMusic[i]) {
-                            band = 1 ;
-                            a =  max + 1 ;
-                            b = max;
-                            
-                            do {
-                                alumMusic[a] = alumMusic[b];
-                                a = a - 1;
-                                b = b - 1;
-                            } while (a == i);
-                        }else{ 
-                            
-                        }
-                    } while (i == max || band == 1);
-                    
-                    max = max + 1;
-                    
-                    if (band == 1 ) {
-                        alumMusic[i] = nombre;
-                    } else {
-                        alumMusic[max] = nombre;
-                    }
-                }
-            }
-            
-            for (int i = 0; i < alumMusic.length; i++) {
-                System.out.println("los alumnos en orden alfabetico" + alumMusic[i]);
-            }  
-        } else {    
-            System.out.println("solo se permiten max. 10 alumnos");
-        }
+//            }
+//            
+//            for (int i = 0; i < alumMusic.length; i++) {
+//                System.out.println("los alumnos en orden alfabetico" + alumMusic[i]);
+//            }  
+//        } else {    
+//            System.out.println("solo se permiten max. 10 alumnos");
+//        }
 
     }
     
@@ -158,9 +245,12 @@ public class Estructuradedatos {
         int prioridad = 0;
         String ExpPOS = null;
         int tope;
+        char operando ;
         tope = 0;
         
-        System.out.println("Ingrese el caractera leer");
+        
+        
+        System.out.println("Ingrese el caracter a leer");
         caracter = teclado.next();
         String [] Pila = new String [tope];
         
@@ -176,7 +266,7 @@ public class Estructuradedatos {
                         
                     }
                 }else{
-                    if (caracter == operando) {  //unn Operando =numeros o letras
+                    if (tope <= 0) {  //un Operando =numeros o letras
                         ExpPOS =  caracter; //se agrga el caracter a ExpPOS
                     }else{  //sino es un operador +-*^
                         while (tope > 0 && prioridad <= tope) {                            
@@ -297,11 +387,11 @@ public class Estructuradedatos {
     // quita el comentario de la tarea a visualizar
     public static void main(String[] args) {
 //        Datos();
-//        separador();
-//        Tarea1_1_1();
+        separador();
+        Tarea1_1_1();
 //        Tarea1_1_2();
 //        Tarea1_2_1(); //pila
-        Tarea1_3_1(); //cola
+//        Tarea1_3_1(); //cola
     
     }
     
