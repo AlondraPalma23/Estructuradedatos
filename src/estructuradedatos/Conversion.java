0c731f2a3fcb54ae4a6ac1433e3d30d4706a6ee7
AlondraPalma23 <al066187@uacam.mx>
2022-02-25 17:30:24 -0600

    new comit 1.2.1

---
+++ src/estructuradedatos/Conversion.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuradedatos;
+
+/**
+ *
+ * @author EduardoPalma
+ */
+public class Conversion {
+    String Qexpre;
+    int indice;
+    
+    //iniciar
+    public Conversion (String expre){
+        this.indice = 0;
+        Qexpre = new String();
+        Qexpre = expre;
+    }
+    
+    //regresar caracter 
+    public String retorno(){
+        String Aux = Qexpre.substring(indice,indice + 1);
+        indice++;
+        return (Aux);
+    }
+    
+    //Agrega caracter al final de qexpresion
+    public void caracterFinal(String car){
+        Qexpre = Qexpre.concat(car);
+        
+    }
+    //contar los carcteres
+    public int Caracteres (){
+        return  Qexpre.length();
+    }
+}
